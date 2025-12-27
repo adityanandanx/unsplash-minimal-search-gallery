137c2cc58a4806e763052adec242248fd502b2c3
adityanandanx <nandanaditya985@gmail.com>
2025-12-28 02:17:31 +0530

    feat: ctrl + k to focus search bar

--- app/components/SearchBar.vue
+++ app/components/SearchBar.vue
@@ -2,14 +2,32 @@
 const query = ref<string>("");
 const router = useRouter();
 
+const inputEl = ref<HTMLInputElement | null>(null);
+
 const handleInput = debounce(() => {
   router.replace(`?search=${query.value}`);
 }, 800);
+
+const handleKeyDown = (e: KeyboardEvent) => {
+  if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "k") {
+    e.preventDefault();
+    inputEl.value?.focus();
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keydown", handleKeyDown);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeyDown);
+});
 </script>
 <template>
   <input
     v-model="query"
     @input="handleInput"
+    ref="inputEl"
     class="border py-2 px-3 rounded-md focus:outline-primary focus:outline"
     type="text"
     placeholder="Start typing... (Ctrl+K to focus)"
